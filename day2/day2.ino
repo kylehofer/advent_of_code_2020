a155a979e6067c5e186a156321fa4162dbafac4c
kyle.hofer <kyle.hofer@conserveitiot.com>
2020-12-07 22:31:25 +1100

    Cleaned up code and added documentation

--- day2/day2.ino
+++ day2/day2.ino
@@ -1,5 +1,11 @@
 #include "day2_input.h"
 
+/**
+ * Reads all sequential integers in a string.
+ * Will translate their value from string to int.
+ * @param passwords A pointer to our progressive pointer that is being moved through the list.
+ * @return An integer representation of the value read from the string.
+ */
 uint8_t read_integer(const char **passwords)
 {
     const char *index = *passwords;
@@ -16,6 +22,14 @@ uint8_t read_integer(const char **passwords)
     return value;
 }
 
+/**
+ * Validates the password by reading two integer values from the string and storing them as a min and max value.
+ * Counts the number of occurance of the key letter inside the password, and compares that to the min and max values.
+ * A password is valid if the occurance of the key character is between the min and max.
+ * A progressive pointer is managed throughout the list.
+ * @param passwords A pointer to our progressive pointer that is being moved through the list.
+ * @return Whether the password is valid or not
+ */
 bool validate_password_old(const char **passwords)
 {
     uint8_t min, max, count = 0;
@@ -26,6 +40,7 @@ bool validate_password_old(const char **passwords)
 
     const char * index = *passwords;
 
+    // Read our key character
     compareValue = pgm_read_byte_near(index);    
 
     index += 3;
@@ -43,6 +58,14 @@ bool validate_password_old(const char **passwords)
     return (count >= min && count <= max);
 }
 
+/**
+ * Validates the password by reading two integer values from the string and storing them as two indexes.
+ * Checks whether the key character occurs at one or the other, but not both of the indexes (XOR).
+ * A password is valid if the key character occurs atleast 1 index, but not both.
+ * A progressive pointer is managed throughout the list.
+ * @param passwords A pointer to our progressive pointer that is being moved through the list.
+ * @return Whether the password is valid or not
+ */
 bool validate_password_new(const char **passwords)
 {
     uint8_t first, second, count = 0;
@@ -74,6 +97,13 @@ bool validate_password_new(const char **passwords)
     return ((firstValue == compareValue) ^ (secondValue == compareValue));
 }
 
+/**
+ * Validates a large string of continous passwords using a password validator function.
+ * Each valid password will increase the counter.
+ * @param data A pointer to the start of the string.
+ * @param func A pointer to the validation function to run on the passwords.
+ * @return The number of valid passwords in the string.
+ */
 uint32_t passwords_validator(const char *data, bool(*func) (const char **))
 {
     uint16_t count = 0;
